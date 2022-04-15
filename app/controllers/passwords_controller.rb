487abb8b25fdaf600e74348be971e54fa8114313
Carlos Henrique <ch.henriquevinha@gmail.com>
2022-04-15 03:11:48 -0300

    fix: controllers

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,8 +1,8 @@
 class PasswordsController < ApplicationController
-
+  before_action :set_user
 
   def index
-    password =  Password.all
+    password =  Password.where(user_id: @current_user)
     render json: password, status: 200
   end
 
@@ -15,5 +15,9 @@ class PasswordsController < ApplicationController
 
   def params_password
     params.require(:password).permit(:name, :master_key, :email, :user_id)
+  end
+
+  def set_user 
+    @current_user = User.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
-  
-  def create 
+
+  def new
+    user = User.new
+  end
+
+  def create
+    #TODO: Refactor to sigin up. Create verifys 
+
     user = User.new(params_user)
-    render json: user if user.save!
+    if user.save!
+      redirect_to '/'
+    else 
+      render new
+    end
   end
 
   private 
-
   def params_user
     params.require(:user).permit(:name, :email, :master_password)
   end
